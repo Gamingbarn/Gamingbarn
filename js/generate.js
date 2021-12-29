ac8094e4a7b5dba69790c643c423bb99e73a2e3c
Adam Griebel <adgriebe@esu10.org>
2021-12-29 16:42:09 -0600

    header and footer work

---
+++ js/generate.js
@@ -0,0 +1,82 @@
+﻿/* https://stackoverflow.com/questions/18712338/make-header-and-footer-files-to-be-included-in-multiple-html-pages */
+/* https://stackoverflow.com/questions/19673398/inserting-script-and-link-tag-inside-the-header-tag */
+
+/* <link rel="stylesheet" href="css/components.css"> */
+if (!document.getElementById('css_components')) {
+    var link = document.createElement('link');
+    link.id = 'css_components';
+    link.rel = 'stylesheet';
+    link.href = '/css/components.css';
+    document.head.appendChild(link);
+}
+
+/* <link rel="stylesheet" href="css/icons.css"> */
+if (!document.getElementById('css_icons')) {
+    var link = document.createElement('link');
+    link.id = 'css_icons';
+    link.rel = 'stylesheet';
+    link.href = '/css/components.css';
+    document.head.appendChild(link);
+}
+
+/* <link rel="stylesheet" href="css/responsee.css"> */
+if (!document.getElementById('css_responsee')) {
+    var link = document.createElement('link');
+    link.id = 'css_responsee';
+    link.rel = 'stylesheet';
+    link.href = '/css/responsee.css';
+    document.head.appendChild(link);
+}
+
+/* <link rel="stylesheet" href="owl-carousel/owl.carousel.css"> */
+if (!document.getElementById('css_owl_carousel')) {
+    var link = document.createElement('link');
+    link.id = 'css_owl_carousel';
+    link.rel = 'stylesheet';
+    link.href = '/owl-carousel/owl.carousel.css';
+    document.head.appendChild(link);
+}
+
+/* <link rel="stylesheet" href="owl-carousel/owl.theme.css"> */
+if (!document.getElementById('css_owl_theme')) {
+    var link = document.createElement('link');
+    link.id = 'css_owl_theme';
+    link.rel = 'stylesheet';
+    link.href = '/owl-carousel/owl.theme.css';
+    document.head.appendChild(link);
+}
+
+/* <link rel="shortcut icon" href="img/logo.png" type="image/x-icon"> */
+if (!document.getElementById('link_logo')) {
+    var link = document.createElement('link');
+    link.id = 'link_logo';
+    link.rel = 'shortcut icon';
+    link.href = '/img/logo.png';
+    link.type = 'image/x-icon'
+    document.head.appendChild(link);
+}
+
+/* <link rel="stylesheet" href="css/template-style.css"> */
+if (!document.getElementById('template_style')) {
+    var link = document.createElement('link');
+    link.id = 'template_style';
+    link.rel = 'stylesheet';
+    link.href = '/css/template-style.css';
+    document.head.appendChild(link);
+}
+
+/*<script type="text/javascript" src="js/jquery-1.8.3.min.js"></script>*/
+//if (!document.getElementById('jquery_main_js')) {
+//    var script = document.createElement('script');
+//    script.id = 'jquery_main_js';
+//    script.src = '/js/jquery-1.8.3.min.js';
+//    document.head.appendChild(script);
+//}
+
+/* <script type="text/javascript" src="js/jquery-ui.min.js"></script> */
+if (!document.getElementById('jquery_ui')) {
+    var script = document.createElement('script');
+    script.id = 'jquery_ui';
+    script.src = '/js/jquery-ui.min.js';
+    document.head.appendChild(script);
+}
